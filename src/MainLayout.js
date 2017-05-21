099ce8ad29957ffd1be700372ea9ecef0f7b6fb7
James Lee <junghlee90@gmail.com>
2017-05-21 06:15:12 -0500

    adding home page and getting rid of App dependency

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -4,8 +4,8 @@ const MainLayout = React.createClass({
   render: function () {
     return (
       <div>
-        <div className="container-fluid">
-          <div className="container-fluid">
+        <div className='container-fluid'>
+          <div className='container-fluid'>
             {this.props.children}
           </div>
         </div>
--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Router, browserHistory, Route, IndexRoute } from 'react-router'
+import { Router, browserHistory, Route } from 'react-router'
 
-import App from './App'
 import HomePage from './pages/home/page'
 import AboutPage from './pages/about/page'
 import MainLayout from './MainLayout'
@@ -10,6 +9,7 @@ export default (
   <Router history={browserHistory}>
     <Route path='/' component={MainLayout}>
       <Route path='/about' component={AboutPage} />
+      <Route path='/home' component={HomePage} />
     </Route>
   </Router>
 )

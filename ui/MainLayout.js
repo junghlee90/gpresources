627c9aa58004ea625f7cf08d29af655c6370e874
James Lee <junghlee90@gmail.com>
2017-05-31 21:26:14 -0500

    creating the main page

--- ui/MainLayout.js
+++ ui/MainLayout.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
+import Paper from 'material-ui/Paper'
+import Menu from 'material-ui/Menu'
+import MenuItem from 'material-ui/MenuItem'
 
 class MainLayout extends Component {
   render () {
+    const style = {
+      display: 'inline-block',
+      margin: '16px 32px 16px 0'
+    }
+
     return (
-      <div>
-        <div className='container-fluid'>
-          <div className='container-fluid'>
-            {this.props.children}
-          </div>
+      <div className='container'>
+        <div className='col-md-2'>
+          <Paper style={style}>
+            <Menu>
+              <MenuItem primaryText='Available' />
+              <MenuItem primaryText='Checked Out' />
+            </Menu>
+          </Paper>
+        </div>
+        <div className='col-md-10'>
+          {this.props.children}
         </div>
       </div>
+
     )
   }
 }

b6cf4837a56654d68327f8a7253cdd52b49627e7
James Lee <junghlee90@gmail.com>
2017-06-13 13:46:32 -0500

    checkouts in reducer

--- ui/modules/resource/ResourceReducer.js
+++ ui/modules/resource/ResourceReducer.js
@@ -1,17 +1,29 @@
 import { combineReducers } from 'redux'
 import {
-  GET_RESOURCE_REQUEST,
   GET_RESOURCE_SUCCESS,
-  GET_RESOURCE_FAILURE,
   ADD_RESOURCE_NEW,
   RESOURCE_NAME_CHANGED,
-  RESOURCE_COUNT_CHANGED
+  RESOURCE_COUNT_CHANGED,
+  UPDATE_CHECKOUT_ITEM,
+  DELETE_CHECKOUT_ITEM
 } from './ResourceActionTypes'
 
 const items = (state = [], action) => {
   switch (action.type) {
     case GET_RESOURCE_SUCCESS:
       return action.resources
+    default:
+      return state
+  }
+}
+
+const checkoutRequest = (state = {}, action) => {
+  switch (action.type) {
+    case UPDATE_CHECKOUT_ITEM:
+      return Object.assign({}, state, {[action.id]: action.count})
+    case DELETE_CHECKOUT_ITEM:
+      let { [action.id]: deletedItem, ...rest } = state
+      return rest
     default:
       return state
   }
@@ -41,6 +53,7 @@ const currentResourceCount = (state = '', action) => {
 
 const resourceReducer = combineReducers({
   items,
+  checkoutRequest,
   currentResourceName,
   currentResourceCount
 })

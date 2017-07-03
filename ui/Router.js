04e79c4fa9987239bad10dd695523cbca717972b
Tom Huynh <nghia.huynh@utexas.edu>
2017-07-03 16:53:13 -0500

    redirect after login
    TODO: make login state and info persistent

--- ui/Router.js
+++ ui/Router.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Router, browserHistory, Route } from 'react-router'
 
-import GoogleLoginWrapper from './login.js'
+//import GoogleLoginWrapper from './login.js'
+import AuthExample from './login.js'
 import ResourcesList from './modules/resource/ResourcesList'
 import ResourceCreate from './modules/resource/ResourceCreate'
 import MainLayout from './MainLayout'
@@ -9,7 +10,8 @@ import MainLayout from './MainLayout'
 export default (
   <Router history={browserHistory}>
     <Route component={MainLayout}>
-      <Route path='/' component={GoogleLoginWrapper} />
+      {/* <Route path='/' component={GoogleLoginWrapper} /> */}
+      <Route path='/' component={AuthExample} />
       <Route path='/home' component={ResourcesList} />
       <Route path='/add_new' component={ResourceCreate} />
     </Route>
--- ui/login.js
+++ ui/login.js
@@ -1,60 +1,134 @@
 var React = require('react');
 import GoogleLogin from 'react-google-login'
-//var GoogleAuth = require('google-auth-library');
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom'
 
-const responseGoogle = (response) => {
-  console.log(response);
-  var profile = response.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  var CLIENT_ID = response.getAuthResponse().id_token;
-  console.log('Validating ID token: ' + CLIENT_ID);
+//based on https://reacttraining.com/react-router/web/example/auth-workflow
 
-  //method 1: tokeninfo endpoint
-  //ok for low traffic
-  //TODO: consider switching to method 2 later
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='+CLIENT_ID);
-  xhr.onload = function() {
-	var res = xhr.response;
-    console.log('response: ' + xhr.response);
-  };
-  xhr.send(null);
-
-  //method 2: using Google API Client Lib (recommended)
-  // build error -> TODO: debug
-  /*
-  var auth = new GoogleAuth;
-  var client = new auth.OAuth2(CLIENT_ID, '', '');
-  var res = client.verifyIdToken(
-    token,
-    CLIENT_ID,
-    // Or, if multiple clients access the backend:
-    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
-    function(e, login) {
-      var payload = login.getPayload();
-      var userid = payload['sub'];
-      // If request specified a G Suite domain:
-      //var domain = payload['hd'];
-    });
-  console.log(res);
-  */
-}
-
-class GoogleLoginWrapper extends React.Component {
-  render(){
+class AuthExample extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {name:''}
+  }
+  updateName = (name) => { this.setState({name:name}); }
+  render() {
     return (
-      <GoogleLogin
-        clientId="635318879150-3u23dekvrtepl6o74p5ru5qrbmhlgh47.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-      />
-	)
+      <Router>
+        <div>
+          <AuthButton name={this.state.name}/>
+          <Route path="/login"
+          render={(props) => <Login {...props} updateName={this.updateName}/>}/>
+          <PrivateRoute path="/" component={Protected}/>
+        </div>
+      </Router>
+    )
   }
 }
 
-module.exports = GoogleLoginWrapper;
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+}
 
+//const AuthButton = withRouter(({ history }) => (
+const AuthButton = (props) => {
+  return(
+    <div>
+    {fakeAuth.isAuthenticated ? (
+      <h3>
+        Welcome, {props.name}!
+      </h3>
+    ) : (
+      <h3>You are not logged in.</h3>
+    )}
+    </div>
+  )
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    fakeAuth.isAuthenticated ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
+
+const Protected = () => <p>Show what I have checked out</p>
+
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { redirectToReferrer: false }
+  }
+
+  responseGoogle = (response) => {
+    var profile = response.getBasicProfile();
+    fakeAuth.authenticate(() => {
+      this.setState({ redirectToReferrer: true })
+    })
+    this.props.updateName(profile.getName())
+
+    //DEBUG
+    console.log(response);
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    var CLIENT_ID = response.getAuthResponse().id_token;
+    console.log('Validating ID token: ' + CLIENT_ID);
+
+    //Google recommends not to pass the ID from the frontend to the backend
+    //but to do something like this instead:
+    //method 1: tokeninfo endpoint
+    //ok for low traffic
+    //TODO: consider switching to method 2 later
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='+CLIENT_ID);
+    xhr.onload = function() {
+      var res = xhr.response;
+      console.log('response: ' + xhr.response);
+    };
+    xhr.send(null);
+  }
+
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    const { redirectToReferrer } = this.state
+
+    if (redirectToReferrer) {
+      return (
+        <Redirect to={from}/>
+      )
+    }
+
+    return (
+      <div>
+        <GoogleLogin
+          clientId="635318879150-3u23dekvrtepl6o74p5ru5qrbmhlgh47.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+        />
+      </div>
+    )
+  }
+}
+
+module.exports = AuthExample;
